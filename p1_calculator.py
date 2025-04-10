67db0647ed3aae5f379fae63ad0b0462165e2fb5
sanjeevmoparthi <kumarsanjeev7101@gmail.com>
2025-04-10 17:30:06 +0000

    succesfully completed the calculator program

---
+++ p1_calculator.py
@@ -0,0 +1,20 @@
+def calculator():
+    no_1 = int(input("enter your first number"))
+    operation = int(input("enter  arithmetic operation  like +,-,*,/"))
+    no_2 = int(input("enter your  second number"))
+
+    if operation =='+':
+        print(f"the sum of {no_1} + {no_2} are {no_1 + no_2}")
+    elif  operation =='-':
+        print(f"the diff of {no_1}  - {no_2} are {no_1 - no_2}")
+    elif operation =='*':
+        print(f"the product of {no_1} * {no_2} are {no_1 * no_2}")
+    elif  operation =='/':
+        if no_2 == 0:
+            print("it gives infinity as it is not divided by zero")
+        else:
+            print(f"the sum of {no_1}  / {no_2} are {no_1 / no_2}")
+    else:
+        print("invalid output")
+
+calculator()

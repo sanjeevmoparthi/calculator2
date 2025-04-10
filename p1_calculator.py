e89120139afc3af20ec7406d600347a18ef5ca44
Ruslan Senatorov <senatorovmotivation@gmail.com>
2025-04-10 20:56:18 +0300

    Update p1_calculator.py

--- p1_calculator.py
+++ p1_calculator.py
@@ -1,20 +1,21 @@
-def calculator():
+def calculator() -> None:
     no_1 = int(input("enter your first number"))
     operation = int(input("enter  arithmetic operation  like +,-,*,/"))
     no_2 = int(input("enter your  second number"))
 
-    if operation =='+':
+    if operation == "+":
         print(f"the sum of {no_1} + {no_2} are {no_1 + no_2}")
-    elif  operation =='-':
+    elif operation == "-":
         print(f"the diff of {no_1}  - {no_2} are {no_1 - no_2}")
-    elif operation =='*':
+    elif operation == "*":
         print(f"the product of {no_1} * {no_2} are {no_1 * no_2}")
-    elif  operation =='/':
+    elif operation == "/":
         if no_2 == 0:
             print("it gives infinity as it is not divided by zero")
         else:
             print(f"the sum of {no_1}  / {no_2} are {no_1 / no_2}")
     else:
         print("invalid output")
+
 
 calculator()
